6d6a1147572e0b544f95bcd9a676cc5aa2c81b63
Pumpkin <wait4pumpkin@gmail.com>
2014-03-26 11:01:49 +0800

    count single buy

--- solution/analysis/single_buy.py
+++ solution/analysis/single_buy.py
@@ -20,7 +20,8 @@ from scipy import sparse
 from scipy.sparse import lil_matrix
 
 from sklearn import svm
-from sklearn import linear_model, cross_validation
+from sklearn import linear_model, cross_validation, datasets
+
 
 N_MONTH = 4
 N_DAY_PER_MONTH = 31
@@ -48,85 +49,10 @@ class User(object):
         return str(self.id) + ' ' + str(len(self.bands))
 
 
-class Graph(object):
-    def __init__(self):
-        self.adjList = dict()
-
-    def add_edge(self, u, v):
-        if u not in self.adjList:
-            self.adjList[u] = set()
-        if v not in self.adjList:
-            self.adjList[v] = set()
-        self.adjList[u].add(v)
-        self.adjList[v].add(u)
-
-    def adj_matrix(self):
-        result = lil_matrix((len(self.adjList), len(self.adjList)))
-        for u in self.adjList:
-            for v in self.adjList[u]:
-                result[u][v] = 1
-
-
-class BFS(object):
-    def __init__(self, graph):
-        self.graph = graph
-        self.components = []
-        isVisited = set()
-        for u in graph.adjList.keys():
-            if u in isVisited: continue
-
-            comp = set()
-            comp.add(u)
-            isVisited.add(u)
-            queue = [u]
-            
-            while len(queue) > 0:
-                u = queue.pop(0)
-                for v in graph.adjList[u]:
-                    if v not in isVisited:
-                        queue.append(v)
-                        isVisited.add(v)
-                        comp.add(v)
-            self.components.append(comp)
-
-    def distance(self, start, end):
-        if start not in self.graph.adjList or \
-            end not in self.graph.adjList: return -1
-
-        self.distances = dict()
-        queue = [start]
-        self.distances[start] = 0
-        while len(queue) > 0:
-            u = queue.pop(0)
-            print 'u', u, self.distances[u]
-            for v in self.graph.adjList[u]:
-                print 'v', v
-                if v not in self.distances:
-                    queue.append(v)
-                    self.distances[v] = self.distances[u] + 1
-                    if v is end: break
-            if end in self.distances: break
-            print '==========================='
-        return self.distances[end] if end in self.distances else -1
-
-# class Dijkstra(object):
-#     def __init(self, graph, start):
-#         self.distances = [sys.maxint for num in range(len(graph.adjList))]
-#         pq = Queue.PriorityQueue()
-#         self.distances[start] = 0
-#         pq.put(start, 0)
-#         while not pq.empty():
-#             u, dist = pq.get()
-#             if dist > self.distances[u]: continue
-#             for v in graph.adjList[u]:
-#                 if self.distances[v] > self.distances[u] + 1:
-#                     self.distances
-
-
 if __name__ == '__main__':
     userInfo = dict()
-    with open('/home/pumpkin/Documents/project/tmall/dataset/t_alibaba_data.csv', 'rb') as csvfile:
-    # with open('/home/pumpkin/Documents/project/tmall/dataset/demo.csv', 'rb') as csvfile:
+    with open('/home/pumpkin/project/tmall/dataset/t_alibaba_data.csv', 'rb') as csvfile:
+    # with open('/home/pumpkin/project/tmall/dataset/dataset/demo.csv', 'rb') as csvfile:
         user_table = dict()
         brand_table = dict()
         user_counter = 0
@@ -166,87 +92,70 @@ if __name__ == '__main__':
     for (userID, info) in userInfo.iteritems():
         users.append(User(userID, info))
 
-    print 'Start: %s' % time.clock()
-
-    g = Graph()
-    data_train = []
+    history = dict()
     for user in users:
-        for month in range(BASE_MONTH, BASE_MONTH + N_MONTH - 1):
-            if month not in user.data: continue
+        if user.id not in history:
+            history[user.id] = dict()
 
-            for brand, actions in user.data[month].items():
-                if actions[1] < 1: continue
-                g.add_edge(user.id, brand + user_counter)
-                data_train.append([user.id, brand + user_counter])
+        for brand_id in user.brands:
+            if brand_id not in history[user.id]:
+                history[user.id][brand_id] = \
+                    { 'view': 0, 'buy': 0, 'label': 0 }
 
-    data_train = numpy.asarray(data_train)
-    print data_train.shape, user_counter, brand_counter
+            history[user.id][brand_id]['view'] = 1 if \
+                sum([sum(user.data[month][brand_id]) - user.data[month][brand_id][1] \
+                    for month in \
+                    xrange(BASE_MONTH, BASE_MONTH + N_MONTH - 1)
+                    if month in user.data and \
+                        brand_id in user.data[month]]) > 0 \
+                    else 0
 
-    prob = 0.9
-    M = lil_matrix((user_counter + brand_counter, user_counter + brand_counter))
-    for i in xrange(user_counter + brand_counter):
-        for j in xrange(user_counter + brand_counter):
-            if i in g.adjList: M[i, j] = prob / len(g.adjList[i])
+            history[user.id][brand_id]['buy'] = 1 if \
+                sum([user.data[month][brand_id][1] for month in \
+                    xrange(BASE_MONTH, BASE_MONTH + N_MONTH - 1)
+                    if month in user.data and \
+                        brand_id in user.data[month]]) > 0 \
+                    else 0
 
-    Q = 1 - prob
+            history[user.id][brand_id]['label'] = \
+                user.data[BASE_MONTH + N_MONTH - 1][brand_id][1] \
+                if BASE_MONTH + N_MONTH - 1 in user.data and \
+                    brand_id in user.data[BASE_MONTH + N_MONTH - 1] \
+                else 0
 
-    n_step = 10
-    k_half = M.dot(Q).dot(M.T * Q)
-    for n in xrange(1, n_step):
-        print n
-        k_half += M.dot(k[n-1]).dot(M.T)
-
-    # print k_half.todense()
-    print k_half.shape
-
-    print 'Graph: %s' % time.clock()
-
-    def graph_kernel(x, y):
-        gram = numpy.empty((x.shape[0], y.shape[0]))
-        for i, u in enumerate(x):
-            for j, v in enumerate(y):
-                gram[i, j] = k_half[u[0], v[0]] * k_half[u[1], v[1]]
-            # for j in xrange(len(data_train)):
-            #     u = data_train[i]
-            #     v = data_train[j]
-            #     gram[i, j] = k_half[u[0], v[0]] * k_half[u[1], v[1]]
-        # return np.dot(x, y.T)
-        # print gram
-        return gram
-
-    clf = svm.OneClassSVM(kernel=graph_kernel)
-    clf.fit(data_train)
-    print Counter(clf.predict(data_train))
-
-    print 'SVM: %s' % time.clock()
-
-
-
-
-
-
-
-    # gram = numpy.empty((len(data_train), len(data_train)))
-    # for i in xrange(len(data_train)):
-    #     for j in xrange(len(data_train)):
-    #         u = data_train[i]
-    #         v = data_train[j]
-    #         gram[i, j] = k_half[u[0], v[0]] * k_half[u[1], v[1]]
-
-    # clf = svm.OneClassSVM(kernel='precomputed')
-    # clf.fit(gram)
-    # print clf
-
-    # # user = users[0]
-    # # data_test = numpy.asarray([[0, 1]])
-    # print clf.predict(gram)
-    # # svm.libsvm.predict_proba
+    view_before = sum([1 for user_id, brands in history.items() \
+        for brand_id, counter in brands.items() \
+        if counter['label'] > 0 and counter['view'] > 0 and counter['buy'] < 1])
+    buy_before = sum([1 for user_id, brands in history.items() \
+        for brand_id, counter in brands.items() \
+        if counter['label'] > 0 and counter['buy'] > 0])
+    buy_total = sum([1 for user_id, brands in history.items() \
+        for brand_id, counter in brands.items() \
+        if counter['label'] > 0])
+    print 'View Not Buy before: ', view_before, buy_total, \
+        '{:.2f}%'.format(float(view_before) / buy_total * 100)
+    print 'Buy before: ', buy_before, buy_total, \
+        '{:.2f}%'.format(float(buy_before) / buy_total * 100)
     
-    # X = numpy.array([[0, 0], [1, 1]])
-    # y = [0, 1]
-    # clf = svm.SVC(kernel='precomputed')
 
-    # gram = numpy.dot(X, X.T)
-    # print clf.fit(gram, y) 
+    # import some data to play with
+    iris = datasets.load_iris()
+    X = iris.data[:, :2]  # we only take the first two features.
+    Y = iris.target
 
-    # print clf.predict(gram)
+    print X.shape
+    print Y.shape
+
+    h = .02  # step size in the mesh
+
+    logreg = linear_model.LogisticRegression(C=1e5)
+
+    # we create an instance of Neighbours Classifier and fit the data.
+    logreg.fit(X, Y)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, m_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
